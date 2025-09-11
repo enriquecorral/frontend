742d837dae7e6794384973ee1111ad1951202142
Enrique Corral <enriquecorral97@gmail.com>
2025-09-10 23:08:09 -0600

    Limpieza

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
-
 function App() {
-
   return (
     <>
-      <h1 className='text-6xl'>Hola Mundo</h1>
+      <h1>Hola Mundo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
