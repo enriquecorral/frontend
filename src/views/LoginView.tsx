d2db90893c431fb748314f75be5d25f128c17bd4
Enrique Corral <enriquecorral97@gmail.com>
2025-09-12 23:46:38 -0600

    Formulario con validación

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,8 +3,15 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <nav>
-        <Link to="/auth/register">¿No tienes cuenta? Crea una aquí</Link>
+      <h1 className="text-4xl text-white font-bold">Iniciar sesión</h1>
+
+      <nav className="mt-10">
+        <Link
+          to="/auth/register"
+          className="text-center text-white text-lg block"
+        >
+          ¿No tienes cuenta? Crea una aquí
+        </Link>
       </nav>
     </>
   );
