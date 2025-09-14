789bf92312bbb7ae1cdcb8e15c40924bccb25caa
Enrique Corral <enriquecorral97@gmail.com>
2025-09-14 14:22:44 -0600

    Agregar Toasts

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios, { isAxiosError } from "axios";
+import { toast } from "sonner";
 import type { RegisterForm } from "../types";
 import ErrorMessage from "../components/ErrorMessage";
 
@@ -29,12 +30,11 @@ export default function RegisterView() {
         `${import.meta.env.VITE_API_URL}/auth/register`,
         formData
       );
-      console.log(data);
-
+      toast.success(data);
       reset();
     } catch (error) {
       if (isAxiosError(error) && error.response) {
-        console.log(error.response?.data.error);
+        toast.error(error.response?.data.error);
       }
     }
   };
