c268ebb747586da74b79a457dce280630e27857b
Enrique Corral <enriquecorral97@gmail.com>
2025-09-14 14:08:14 -0600

    Manejar Registro de usuarios

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import axios, { isAxiosError } from "axios";
+import type { RegisterForm } from "../types";
 import ErrorMessage from "../components/ErrorMessage";
 
 export default function RegisterView() {
@@ -14,14 +16,27 @@ export default function RegisterView() {
   const {
     register,
     watch,
+    reset,
     handleSubmit,
     formState: { errors },
-  } = useForm({ defaultValues: initialValues });
+  } = useForm<RegisterForm>({ defaultValues: initialValues });
 
   const password = watch("password");
 
-  const handleRegister = () => {
-    console.log("Desde handleRegister");
+  const handleRegister = async (formData: RegisterForm) => {
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/register`,
+        formData
+      );
+      console.log(data);
+
+      reset();
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        console.log(error.response?.data.error);
+      }
+    }
   };
 
   return (
