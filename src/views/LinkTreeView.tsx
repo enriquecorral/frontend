7eaa2d82fca83b06e31cd8fc1bac55eb702a8282
Enrique Corral <enriquecorral97@gmail.com>
2025-10-05 15:09:34 -0600

    Mostrando el formulario de redes sociales

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,3 +1,17 @@
+import { useState } from "react";
+import { social } from "../data/social";
+import DevTreeInput from "../components/DevTreeInput";
+
 export default function LinkTreeView() {
-  return <div>LinkTreeView</div>;
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+  return (
+    <>
+      <div className="space-y-5">
+        {devTreeLinks.map((item) => (
+          <DevTreeInput key={item.name} item={item} />
+        ))}
+      </div>
+    </>
+  );
 }
