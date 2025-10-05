dd0fe07b58c3572a38006b6649fa67a48ad18083
Enrique Corral <enriquecorral97@gmail.com>
2025-10-05 15:45:05 -0600

    Detectando si una URL es válida antes de habilitarla

--- src/components/DevTreeInput.tsx
+++ src/components/DevTreeInput.tsx
@@ -4,20 +4,32 @@ import { classNames } from "../utils";
 
 type DevTreeInputProps = {
   item: DevTreeLink;
+  handleUrlChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleEnableLink: (socialNetwork: string) => void;
 };
 
-export default function DevTreeInput({ item }: DevTreeInputProps) {
+export default function DevTreeInput({
+  item,
+  handleUrlChange,
+  handleEnableLink,
+}: DevTreeInputProps) {
   return (
     <div className="bg-white shadow-sm p-5 flex items-center gap-3">
       <div
         className="w-12 h-12 bg-cover"
         style={{ backgroundImage: `url(/social/icon_${item.name}.svg)` }}
       ></div>
-      <input type="text" className="flex-1 border border-gray-100 rounded-lg" />
+      <input
+        type="text"
+        className="flex-1 border border-gray-100 rounded-lg"
+        value={item.url}
+        onChange={handleUrlChange}
+        name={item.name}
+      />
 
       <Switch
         checked={item.enabled}
-        onChange={() => {}}
+        onChange={() => handleEnableLink(item.name)}
         className={classNames(
           item.enabled ? "bg-blue-500" : "bg-gray-200",
           "relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -1,15 +1,44 @@
 import { useState } from "react";
 import { social } from "../data/social";
 import DevTreeInput from "../components/DevTreeInput";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
 
 export default function LinkTreeView() {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link
+    );
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map((link) => {
+      if (link.name === socialNetwork) {
+        if (isValidUrl(link.url)) {
+          return { ...link, enabled: !link.enabled };
+        } else {
+          toast.error("URL no válida");
+          return link;
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
 
   return (
     <>
       <div className="space-y-5">
         {devTreeLinks.map((item) => (
-          <DevTreeInput key={item.name} item={item} />
+          <DevTreeInput
+            key={item.name}
+            item={item}
+            handleUrlChange={handleUrlChange}
+            handleEnableLink={handleEnableLink}
+          />
         ))}
       </div>
     </>
